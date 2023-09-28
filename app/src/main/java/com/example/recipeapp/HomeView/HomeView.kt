1a150df0b6e85697fde2c278effce749269641f6
Emil <emilsuuronen@gmail.com>
2023-09-28 14:34:34 +0300

    add card styling

--- app/src/main/java/com/example/recipeapp/HomeView/HomeView.kt
+++ app/src/main/java/com/example/recipeapp/HomeView/HomeView.kt
@@ -2,7 +2,9 @@ package com.example.recipeapp.HomeView
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,9 +13,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.BottomAppBar
@@ -31,8 +36,15 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -60,9 +72,8 @@ fun HomeView() {
         content = {
             NavHost(
                 navController = navController,
-                startDestination = "home" // Set the starting destination
+                startDestination = "home"
             ) {
-                // Define your navigation graph here
                 composable("home") {
                     HomeListView()
                 }
@@ -77,9 +88,7 @@ fun HomeListView() {
     val itemList = listOf(
         Item(name = "Egg", quantity = "7 qty", expiryDate = "19.9.2023"),
         Item(name = "Egg", quantity = "7 qty", expiryDate = "19.9.2023"),
-        Item(name = "Egg", quantity = "7 qty", expiryDate = "19.9.2023"),
-        Item(name = "Egg", quantity = "7 qty", expiryDate = "19.9.2023"),
-        Item(name = "Egg", quantity = "7 qty", expiryDate = "19.9.2023"),
+        Item(name = "Egg", quantity = "7 qty", expiryDate = "19.9.2023")
     )
 
     Column(
@@ -87,12 +96,14 @@ fun HomeListView() {
             .fillMaxSize()
             .padding(4.dp)
     ) {
-        // First LazyColumn, filling half of the screen
         LazyColumn(
             modifier = Modifier
                 .weight(1f)
                 .fillMaxHeight()
         ) {
+            item {
+                Text(text = "In your fridge")
+            }
             items(itemList) { item ->
                 ItemCard(
                     itemName = item.name,
@@ -114,35 +125,105 @@ fun HomeListView() {
     }
 }
 @Composable
-fun ItemCard(itemName: String, itemQuantity: String, expiryDate: String) {
-    ElevatedCard(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        ),
+fun ItemCard(
+    itemName: String,
+    itemQuantity: String,
+    expiryDate: String,
+) {
+    Card(
         modifier = Modifier
             .padding(12.dp)
-    ) {
-        Row (modifier = Modifier
             .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween){
-            Column(modifier = Modifier.padding(all = 8.dp)) {
-                Text(text = itemName, fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(text = itemQuantity)
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(text = "Expiring in $expiryDate", modifier = Modifier
-                    .padding(10.dp))
-            }
-            Card(modifier = Modifier
-                .padding(8.dp)
-                .widthIn(0.dp, 100.dp)) {
-                Image(
-                    painter = painterResource(R.drawable.egg),
-                    contentDescription = "Product image",
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            // Background Image
+            Image(
+                painter = painterResource(id = R.drawable.egg), // Replace with your image resource
+                contentDescription = "Product image",
+                contentScale = ContentScale.Crop,
+                colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply{setToScale(0.5f,0.5f,0.5f,1f)}),
+                modifier = Modifier
+                    .height(200.dp)
+                    .blur(
+                        radiusX = 5.dp,
+                        radiusY = 5.dp,
+                    )
+            )
+
+            // Card Content
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize()
+            ) {
+                Row(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(0.dp)
+                        .padding(bottom = 8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = itemName,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(end = 8.dp)
+                    )
+                }
+
+                Text(
+                    text = itemQuantity,
+                    fontSize = 16.sp,
+                    color = Color.White
                 )
+
+                Text(
+                    text = "Expiring in $expiryDate",
+                    modifier = Modifier.padding(top = 8.dp),
+                    color = Color.White
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(
+                        onClick = { /* TODO */ },
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(36.dp)
+                            .padding(end = 4.dp), // Add padding here
+                        colors = ButtonDefaults.buttonColors(
+                            colors.primary,
+                            contentColor = Color.White
+                        )
+                    ) {
+                        Text(text = "Edit")
+                    }
+
+                    Button(
+                        onClick = { /* TODO */ },
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(36.dp)
+                            .padding(start = 4.dp), // Add padding here
+                        colors = ButtonDefaults.buttonColors(
+                            colors.error,
+                            contentColor = Color.White
+                        )
+                    ) {
+                        Text(text = "Remove")
+                    }
+                }
             }
         }
     }
