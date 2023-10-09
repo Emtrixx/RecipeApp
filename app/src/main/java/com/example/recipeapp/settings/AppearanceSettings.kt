a4f12ebd3d05bd30390b141cea1f1f361cff9473
Jesse Guenzl <contact@jesseguenzl.com>
2023-10-09 19:28:04 +0300

    Fix: icons for Settings pages

--- app/src/main/java/com/example/recipeapp/settings/AppearanceSettings.kt
+++ app/src/main/java/com/example/recipeapp/settings/AppearanceSettings.kt
@@ -2,6 +2,8 @@ package com.example.recipeapp.settings
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import com.example.recipeapp.settings.components.SettingItem
 import com.example.recipeapp.settings.components.SettingsLayout
@@ -10,6 +12,13 @@ import com.example.recipeapp.settings.components.SettingsLayout
 fun AppearanceSettings() {
     SettingsLayout(
         "Appearance", "Appearance Settings for the app",
-        { SettingItem(name = "Theme", icon = { Icons.Default.Face }, onClick = { }) },
+        {
+            SettingItem(name = "Theme", icon = {
+                Icon(
+                    Icons.Default.Face,
+                    contentDescription = "Theme settings"
+                )
+            }, onClick = { })
+        },
     )
 }
--- app/src/main/java/com/example/recipeapp/settings/DevSettings.kt
+++ app/src/main/java/com/example/recipeapp/settings/DevSettings.kt
@@ -2,6 +2,8 @@ package com.example.recipeapp.settings
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import com.example.recipeapp.settings.components.SettingItem
 import com.example.recipeapp.settings.components.SettingsLayout
@@ -10,6 +12,13 @@ import com.example.recipeapp.settings.components.SettingsLayout
 fun DevSettings() {
     SettingsLayout(
         "Dev", "Developer Settings for the app",
-        { SettingItem(name = "Test", icon = { Icons.Default.Info }, onClick = { }) },
+        {
+            SettingItem(name = "Test", icon = {
+                Icon(
+                    Icons.Default.Info,
+                    contentDescription = "Test settings"
+                )
+            }, onClick = { })
+        },
     )
 }
