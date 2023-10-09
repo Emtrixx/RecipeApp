96b5a82d767c4069c253ab710fa5cfc9202c210e
Jesse Guenzl <contact@jesseguenzl.com>
2023-10-09 21:35:21 +0300

    Feat: Add PressedColorButton and demo it on Dev settings page

--- app/src/main/java/com/example/recipeapp/settings/DevSettings.kt
+++ app/src/main/java/com/example/recipeapp/settings/DevSettings.kt
@@ -2,9 +2,9 @@ package com.example.recipeapp.settings
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import com.example.recipeapp.components.PressedColorButton
 import com.example.recipeapp.settings.components.SettingItem
 import com.example.recipeapp.settings.components.SettingsLayout
 
@@ -20,5 +20,8 @@ fun DevSettings() {
                 )
             }, onClick = { })
         },
+        {
+            PressedColorButton()
+        }
     )
 }
